b1a4b077a22d8731ab441f331e1121653f175b2e
Harsheem <hashimnabuyobo@gmail.com>
2023-08-14 07:41:40 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return int 
+ * use the puts
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
