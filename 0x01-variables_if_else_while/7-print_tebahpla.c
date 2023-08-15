34aad8e990ae9a38bad26a0da0330dc5b643c64c
Harsheem <hashimnabuyobo@gmail.com>
2023-08-14 17:44:56 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}		
