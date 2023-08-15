987b4ae1850c691ca735ce5c04776e35388f4ea1
Harsheem <hashimnabuyobo@gmail.com>
2023-08-14 17:00:37 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char letter, e, q;
+	letter = 'a';
+	e = 'e';
+	q = 'q';
+
+	while (letter <= 'z')
+	{
+		if (letter != e && letter != q)
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
