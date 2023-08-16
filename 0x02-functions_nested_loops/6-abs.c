e1e7eae1721d503ff593abed06583e06193fb67b
Harsheem <hashimnabuyobo@gmail.com>
2023-08-16 15:06:26 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 /**
  * _abs - computes  absolute value of an integer
  *
- * @c: The number to be computed
- * Returns: Absolute value of a number
+ * @n: The number to be computed
+ * Return: Absolute value of a n
  */
 int _abs(int n)
 {
