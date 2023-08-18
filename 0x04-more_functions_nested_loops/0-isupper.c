16b4548a6fc33ab1a3da8f50906c30b7c1404bc6
Harsheem <hashimnabuyobo@gmail.com>
2023-08-18 01:01:28 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper(c) - function that checks Uppercase character
+ *
+ * @c: character to be checked
+ * Return: 1 if upperrcase otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c < 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
