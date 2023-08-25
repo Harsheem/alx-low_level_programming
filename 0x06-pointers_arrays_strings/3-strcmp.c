697a56e575cdff8c59e22342ebb755177f986387
Harsheem <hashimnabuyobo@gmail.com>
2023-08-25 15:59:28 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
 	{
-		s1++;
-		s2++;
+		return (0);
 	}
-	if (*s1 < *s2)
-	{
-		return (-15);
-	}
-	else if (*s1 > *s2)
-	{
-		return (15);
-	}
-	return (0);
+	return (*s1 - *s2);
 }
