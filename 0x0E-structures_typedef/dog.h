fca18256dfdc0fa2ee613cf6aff4fcb681501862
Harsheem <hashimnabuyobo@gmail.com>
2023-09-12 17:01:30 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef OBJ_DOG
+#define OBJ_DOG
+
+/**
+ * struct dog - creates a structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a structure thatdefines dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* OBJ_DOG */
