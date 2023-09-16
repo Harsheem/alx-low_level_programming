1bc3cf82e66f4c748f28e6eeae860640b0168cda
Harsheem <hashimnabuyobo@gmail.com>
2023-09-15 17:44:08 -0700

    1-array_iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
+#include <stddef.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* HEADE_FILE */
