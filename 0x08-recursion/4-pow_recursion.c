d34b9c0c305dda48a0c34e3db68543633707f516
Harsheem <hashimnabuyobo@gmail.com>
2023-11-16 15:43:28 -0800

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - retuns the value of x raised to the power of y
+ *
+ * @x: base parameter
+ * @y: exponent parameter
+ *
+ * Return: value of x raised to the power of y or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
