7afbe343dcaa696545ff3d7e016eaa63672c343d
Harsheem <hashimnabuyobo@gmail.com>
2023-11-20 03:21:08 -0800

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *ptr;
 
 	if (size == 0)
@@ -25,6 +26,10 @@ char *create_array(unsigned int size, char c)
 		}
 		else
 		{
+			for (i = 0; i < size; i++)
+			{
+				ptr[i] = c;
+			}
 			return (ptr);
 		}
 	}
